9d855658d9455af1089bb287b354a199c983c905
Vineet Jha <vineet.jha@sparkspite.com>
2024-08-27 12:02:32 +0530

    - (add) GIt Hub Actions

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mishop/modules/base_module/screen/bottom_navigation_bar.dart';
-import 'package:mishop/modules/cart_module/screen/my_cart_view.dart';
-import 'package:mishop/modules/cart_module/screen/my_shopping_list_view.dart';
-import 'package:mishop/modules/cart_module/screen/review_view.dart';
-import 'package:mishop/modules/category_module/categorySubModules/product_view.dart';
-import 'package:mishop/modules/category_module/screen/category_search_view.dart';
-import 'package:mishop/modules/category_module/categorySubModules/category_view.dart';
-import 'package:mishop/modules/cart_module/screen/checkout_view.dart';
-import 'package:mishop/modules/order_info_module/screen/order_details.dart';
-import 'package:mishop/modules/profile_module/screen/my_account.dart';
-import 'package:mishop/modules/splash_module/screen/language.dart';
 import 'package:mishop/modules/splash_module/screen/splash_Screen.dart';
+import 'package:mishop/utils/video_player/video_tapioca.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -29,7 +19,7 @@ class MyApp extends StatelessWidget {
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      home: VideoEditorScreen(),
     );
   }
 }
